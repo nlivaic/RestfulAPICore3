6f0df45283b706712a2d793da553a70b598adaa1
nenadlivaic <nenad.livaic@typeqast.com>
2020-01-07 12:48:12 +0100

    dev: Route incoming requests based on request header and media type.

--- RestfulAPICore3.API/Constraints/RequestHeaderMatchesMediaTypeAttribute.cs
+++ RestfulAPICore3.API/Constraints/RequestHeaderMatchesMediaTypeAttribute.cs
@@ -10,31 +10,38 @@ namespace RestfulAPICore3.API.Constraints
     public class RequestHeaderMatchesMediaTypeAttribute : Attribute, IActionConstraint
     {
         private readonly string _requestHeader;
-        private readonly List<string> _mediaTypeValues = new List<string>();
+        private readonly IList<MediaTypeHeaderValue> _mediaTypeValues = new List<MediaTypeHeaderValue>();
 
         public int Order => 0;
 
-        public RequestHeaderMatchesMediaTypeAttribute(string requestHeader, string mediaTypeValues, params string[] otherMediaTypeValues)
+        public RequestHeaderMatchesMediaTypeAttribute(string requestHeader, string mediaTypeValue, params string[] otherMediaTypeValues)
         {
             _requestHeader = requestHeader;
-            _mediaTypeValues.Add(mediaTypeValues);
-            _mediaTypeValues.AddRange(otherMediaTypeValues);
+            if (otherMediaTypeValues.Length > 0 && !MediaTypeHeaderValue.TryParseList(otherMediaTypeValues, out _mediaTypeValues))
+            {
+                throw new ArgumentException($"Invalid media types: {string.Join(',', otherMediaTypeValues)} declared in {nameof(RequestHeaderMatchesMediaTypeAttribute)}.");
+            }
+            if (!MediaTypeHeaderValue.TryParse(mediaTypeValue, out MediaTypeHeaderValue mediaTypeHeaderValue))
+            {
+                throw new ArgumentException($"Invalid media type: {mediaTypeValue} declared in {nameof(RequestHeaderMatchesMediaTypeAttribute)}.");
+            }
+            _mediaTypeValues.Add(mediaTypeHeaderValue);
         }
 
         public bool Accept(ActionConstraintContext context)
         {
-            if (!context.RouteContext.HttpContext.Request.Headers.TryGetValue(_requestHeader, out StringValues mediaTypeStringValue))
+            if (!context.RouteContext.HttpContext.Request.Headers.TryGetValue(_requestHeader, out StringValues mediaTypeStringValueFromRequest))
             {
                 return false;
             }
-            var mediaTypes = mediaTypeStringValue.ToString().Split(",");
+            var mediaTypes = mediaTypeStringValueFromRequest.ToString().Split(",");
             IList<MediaTypeHeaderValue> parsedMediaTypes = new List<MediaTypeHeaderValue>();
             if (!MediaTypeHeaderValue.TryParseList(mediaTypes, out parsedMediaTypes))
             {
                 return false;
             }
             return _mediaTypeValues.Any(
-                m => mediaTypes.Contains(m));
+                m => mediaTypes.Contains(m.ToString()));
         }
     }
 }
